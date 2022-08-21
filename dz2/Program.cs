e1e5fa1b24b42cad98cfb15c7155eb0a063290f2
Kashira2 <nekit.egorov@icloud.com>
2022-08-21 14:47:56 +0300

    Last commit

---
+++ dz2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int N3 = Convert.ToInt32(Console.ReadLine());
+int max = N1;
+if (N2 > max)
+{
+    max = N2;
+}
+if (N3 > max)
+{
+     max = N3;
+}
+Console.WriteLine(max);
